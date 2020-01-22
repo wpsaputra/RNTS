19daae9bf642986e54f4f1160ab48a638ac1a0a8
wayan.saputra <wayan.saputra@bps.go.id>
2020-01-22 14:00:27 +0800

    fix login

--- screens/Login.jsx
+++ screens/Login.jsx
@@ -8,7 +8,7 @@ export default class Login extends Component {
         headerShown: false,
     };
 
-    StarIcon = (style) => (
+    GoogleIcon = (style) => (
         <Icon {...style} name="google"/>
     );
     
@@ -40,14 +40,14 @@ export default class Login extends Component {
                     </View>
 
                     <View style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between", paddingLeft: 20, paddingRight: 20}}>
-                        <View style={{width: "45%", height: 1, backgroundColor: "gray", overflow: "hidden"}}/>
+                        <View style={{width: "45%", height: 1, backgroundColor: "#d0d0d0", overflow: "hidden"}}/>
                         <Text>OR</Text>
-                        <View style={{width: "45%", height: 1, backgroundColor: "gray", overflow: "hidden"}}/>
+                        <View style={{width: "45%", height: 1, backgroundColor: "#d0d0d0", overflow: "hidden"}}/>
                     </View>
 
                     <View style={{paddingLeft: 20, paddingRight: 20, display: "flex", alignItems: "center", flexDirection: "column", marginBottom: 90}}>
                         {/* <Button style={{backgroundColor: "#28a745", borderColor: "#28a745"}}>Create New Facebook Account</Button> */}
-                        <Button status="success" icon={this.StarIcon}>Sign In with Gooogle</Button>
+                        <Button status="success" icon={this.GoogleIcon} onPress={()=>{this.props.navigation.navigate('HomeNav')}}>Sign In with Gooogle</Button>
                         {/* <Icon name="star"/> */}
                         {/* <Icon name='star' width={32} height={32} fill='#3366FF'/> */}
                     </View>
